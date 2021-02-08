bb831bef6bc6918c0cfea3b8b570aa6055df9be2
Kulbhushan <kulbhushan.mayer@>
2021-02-08 16:32:32 +0530

    fetch route table info for default VPC and added new pcx route to the same

--- terraform-vpc/main.tf
+++ terraform-vpc/main.tf
@@ -113,3 +113,17 @@ resource "aws_vpc_peering_connection" "pc" {
   }
   auto_accept = true
 }
+
+data "aws_route_table" "default"{
+  vpc_id = data.aws_vpc.default.id
+  filter {
+    name = "tag:Name"
+    values = ["default"]
+  }
+}
+
+resource "aws_route" "pc_route" {
+  route_table_id = data.aws_route_table.default.id
+  destination_cidr_block = aws_vpc.vpc.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.pc.id
+}
