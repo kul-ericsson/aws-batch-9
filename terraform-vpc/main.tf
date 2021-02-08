dbffbbc3d8dfaade1135dac2e2352e942b25b4d7
Kulbhushan <kulbhushan.mayer@>
2021-02-08 16:16:42 +0530

    creating IGW, Route Table with IGW Route & Subnet Association for Public Subnet

--- terraform-vpc/main.tf
+++ terraform-vpc/main.tf
@@ -33,3 +33,26 @@ resource "aws_subnet" "sn-3" {
     "Name" = "thinknyx-${var.tagName}-private"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    "Name" = "thinknyx-${var.tagName}"
+  }
+}
+
+resource "aws_route_table" "igw_rt" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    "Name" = "thinknyx-${var.tagName}-igw"
+  }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "igw_rt" {
+  subnet_id = aws_subnet.sn-1.id
+  route_table_id = aws_route_table.igw_rt.id
+}
