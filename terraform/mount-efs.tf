60a5c9aef2e4f6a646b4c6750591ccf9b219b5e3
Kulbhushan <kulbhushan.mayer@>
2021-02-08 15:37:37 +0530

    added null resource with provisioner to install Terraform cli

--- terraform/mount-efs.tf
+++ terraform/mount-efs.tf
@@ -14,3 +14,20 @@ resource "null_resource" "mount_efs" {
     ]
   }
 }
+
+resource "null_resource" "setup_terraform" {
+  provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      user = "ubuntu"
+      private_key = file("C:/training/ericsson/aws/kul-ericsson-thinknyx.pem")
+      host = aws_instance.kul.public_ip
+    }
+    inline = [
+      "sudo apt-get install -y unzip",
+      "wget https://releases.hashicorp.com/terraform/0.14.6/terraform_0.14.6_linux_amd64.zip && unzip terraform_0.14.6_linux_amd64.zip",
+      "sudo cp terraform /usr/bin && sudo chmod 755 /usr/bin/terraform",
+      "terraform --version"
+    ]
+  }
+}
