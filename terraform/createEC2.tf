323ac547c0d5ce5757b89e9c6b4be9c46c410fde
Kulbhushan <kulbhushan.mayer@>
2021-02-08 12:50:46 +0530

    using data source to fetch default VPC details

--- terraform/createEC2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "kul" {
-  ami = "ami-0b3dec44fdc13daaf"
-  instance_type = "t2.micro"
-  key_name = "kul-ericsson-thinknyx"
-  tags = {
-    "Name" = "thinknyx-${var.tagName}"
-  } 
-}
