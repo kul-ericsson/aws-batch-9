205b904494b90d21a2befd7e53e8bf0818966283
Kulbhushan <kulbhushan.mayer@>
2021-02-08 12:50:02 +0530

    updated to print VPC & SG ID as outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,9 @@ output "instance_id" {
 output "apache_url" {
   value = "http://${aws_instance.kul.public_dns}"
 }
+output "default_vpc_id" {
+  value = data.aws_vpc.vpc.id
+}
+output "security_group_id" {
+  value = aws_security_group.sg.id
+}
